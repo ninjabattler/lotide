5a61aebace50bbb632d814cf0f3a0fe27da036a3
Jayden Tucker <jaydenrtucker@gmail.com>
2020-08-17 15:21:18 -0700

    Function that log if two arguments are equal or not

---
+++ assertEqual.js
@@ -0,0 +1,19 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+
+    console.log("✅Assertion Passed: " + actual + " === " + expected);
+
+  } else {
+
+    console.log("🛑Assertion Failed: " + actual + " !== " + expected);
+
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("yes", "yes");
+assertEqual(1, 1);
