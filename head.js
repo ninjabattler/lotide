9cf9d4e3d68fb97d748a91cca1abdfde2b5f422e
ninjabattler <jaydenrtucker@gmail.com>
2020-08-24 19:23:27 +0000

    Refactored both files to export the functionsusing module.exports

--- head.js
+++ head.js
@@ -1,16 +1,4 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-
-  } else {
-
-    console.log(`🛑Assertion Failed: ${actual} === ${expected}`);
-
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(input){
 
@@ -18,5 +6,4 @@ const head = function(input){
 
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
